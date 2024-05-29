de45acb49c98b19821fcaa36853720bf3a9ade32
achraflafkiri <achraf.lafkiri.2@gmail.com>
2024-05-29 14:26:21 +0100

    blog project first commit

---
+++ Frontend/src/components/GeneralScreens/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../Css/Footer.css'
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="footer">
+            </div>
+            <div className="copyright">
+                <p className="copyright-blog">© 2024 <a href="https://www.lafkiri.com/" target='_blank'>
+                    Achraf LAFKIRI</a>. All Rights Reserved</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
